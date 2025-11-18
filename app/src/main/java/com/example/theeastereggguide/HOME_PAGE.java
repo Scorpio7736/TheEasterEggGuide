c506ae9975a987e97d15ebb66ff59759afddf09d
rickugor <rickugorji780@gmail.com>
2025-11-18 16:11:50 -0600

    Message box in home screen

--- app/src/main/java/com/example/theeastereggguide/HOME_PAGE.java
+++ app/src/main/java/com/example/theeastereggguide/HOME_PAGE.java
@@ -1,7 +1,9 @@
 package com.example.theeastereggguide;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -11,6 +13,13 @@ public class HOME_PAGE extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+
+        SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
+        boolean firstStart = prefs.getBoolean("firstStart", true);
+
+        if (firstStart) {
+            showFirstTimeDialog();
+        }
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setItemIconTintList(null);
@@ -25,5 +34,19 @@ public class HOME_PAGE extends AppCompatActivity {
             }
             return false;
         });
+    }
+
+    private void showFirstTimeDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle("Welcome!")
+                .setMessage("Welcome to the Easter Egg Guide")
+                .setPositiveButton("OK", (dialog, which) -> {
+                    SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
+                    SharedPreferences.Editor editor = prefs.edit();
+                    editor.putBoolean("firstStart", false);
+                    editor.apply();
+                    dialog.dismiss();
+                })
+                .create().show();
     }
 }
