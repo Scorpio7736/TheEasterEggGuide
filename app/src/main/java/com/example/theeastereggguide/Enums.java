e0b8483e46615bae5f4e28e62b55772646ea0749
Jack <Jack@thelondons.com>
2025-12-08 15:10:50 -0600

    Added the rest of the music files and fixed their relative Enum.

--- app/src/main/java/com/example/theeastereggguide/Enums.java
+++ app/src/main/java/com/example/theeastereggguide/Enums.java
@@ -4,10 +4,10 @@ public class Enums {
 
     public enum App_Music
     {
-        Damned_Black_Ops_1("Damned Black Ops 1", R.raw.easter_egg_found),
+        Damned_Black_Ops_1("Damned Black Ops 1", R.raw.damned_boi),
         Damned_Black_Ops_2("Damned Black Ops 2", R.raw.damned_boii),
-        Damned_Black_Ops_3("Damned Black Ops 3", R.raw.easter_egg_found),
-        Damned_Black_Ops_4("Damned Black Ops 4", R.raw.easter_egg_found);
+        Damned_Black_Ops_3("Damned Black Ops 3", R.raw.damned_boiii),
+        Damned_Black_Ops_4("Damned Black Ops 4", R.raw.damned_boiiii);
 
         private final String displayName;
         private final int musicResId;
